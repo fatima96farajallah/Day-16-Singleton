d03260946112e2c215657225f2ab10e7f73801a7
FATIMA <farajallahmfatima@gmail.com>
2019-08-07 00:01:42 +0300

    do hachCode the same

--- src/Forms/MainForm.java
+++ src/Forms/MainForm.java
@@ -1,61 +1,52 @@
-
-/*
- *
- *  Copyright (C) 2019.  PSEU - Palestine Polytechnic University - All Rights Reserved
- *  * SPDX-License-Identifier: Apache-2.0
- *
- */
-
-/*
- * Created by Basel.Altamimi on 8/4/2019.
- */
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
 
 package Forms;
 
-import javax.swing.*;
-import java.awt.*;
+import Util.Logger;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class MainForm extends JFrame {
+    int hashcode1;
+    int hashcode2;
+
     public MainForm() {
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Close the application on JForm exit
+        this.setDefaultCloseOperation(3);
         this.setTitle("Main Form");
         this.setSize(600, 200);
-        this.setLayout(new FlowLayout(FlowLayout.RIGHT));
+        this.setLayout(new FlowLayout(2));
         this.setVisible(true);
-
-        JButton b1 = new JButton("Show Calender 1"); // Button 1 (Create Calender 1)
+        JButton b1 = new JButton("Show Calender 1");
         b1.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent ae) {
-                showNewCalender();
+                MainForm.this.showNewCalender();
             }
         });
-
-        JButton b2 = new JButton("Show Calender 2"); // Button 2 (Create Calender 2)
+        JButton b2 = new JButton("Show Calender 2");
         b2.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent ae) {
-                showNewCalender();
+                MainForm.this.showNewCalender();
             }
         });
-
-
         JPanel panel = new JPanel();
-        panel.setLayout(new FlowLayout(FlowLayout.RIGHT));
+        panel.setLayout(new FlowLayout(2));
         panel.add(b1);
         panel.add(b2);
-
         this.add(panel);
     }
 
-    /**
-     * Create and show new calender object
-     * Todo: This method logs the object HashCode in a text file, after refactoring the code; show warning message if the HashCode of Calender1 doesn't equal Calender2 HashCode
-     */
-    private void showNewCalender() {
-        SwingCalendar sc = new SwingCalendar();
-        Util.Logger.log("Object HC: " + sc.hashCode()); // Log Calender hash code
+    public int showNewCalender() {
+        SwingCalendar sc = SwingCalendar.getInstance();
+        Logger.log("Object HC: " + sc.hashCode());
+        sc.setVisible(true);
+        int hash = sc.hashCode();
+        return hash;
     }
-
-
 }
--- src/Forms/SwingCalendar.java
+++ src/Forms/SwingCalendar.java
@@ -1,99 +1,89 @@
-
-/*
- *
- *  Copyright (C) 2019.  PSEU - Palestine Polytechnic University - All Rights Reserved
- *  * SPDX-License-Identifier: Apache-2.0
- *
- */
-
-/*
- * Class source: https://www.javacodex.com/Swing/Swing-Calendar
- * Created by Basel.Altamimi on 8/4/2019.
- */
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
 
 package Forms;
 
-import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
-import java.awt.*;
+import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Locale;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 public class SwingCalendar extends JFrame {
-
+    public static SwingCalendar hashcodetest;
     DefaultTableModel model;
     Calendar cal = new GregorianCalendar();
     JLabel label;
 
-    // Todo: Refactor this constructor so the program will create only one copy of this object
-    SwingCalendar() { // Constructor
-
-        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    SwingCalendar() {
+        this.setDefaultCloseOperation(2);
         this.setTitle("Swing Calendar");
         this.setSize(300, 200);
         this.setLayout(new BorderLayout());
         this.setVisible(true);
-
-
-        label = new JLabel();
-        label.setHorizontalAlignment(SwingConstants.CENTER);
-
+        this.label = new JLabel();
+        this.label.setHorizontalAlignment(0);
         JButton b1 = new JButton("<-");
         b1.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent ae) {
-                cal.add(Calendar.MONTH, -1);
-                updateMonth();
+                SwingCalendar.this.cal.add(2, -1);
+                SwingCalendar.this.updateMonth();
             }
         });
-
         JButton b2 = new JButton("->");
         b2.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent ae) {
-                cal.add(Calendar.MONTH, +1);
-                updateMonth();
+                SwingCalendar.this.cal.add(2, 1);
+                SwingCalendar.this.updateMonth();
             }
         });
-
         JPanel panel = new JPanel();
         panel.setLayout(new BorderLayout());
-        panel.add(b1, BorderLayout.WEST);
-        panel.add(label, BorderLayout.CENTER);
-        panel.add(b2, BorderLayout.EAST);
-
-
-        String[] columns = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
-        model = new DefaultTableModel(null, columns);
-        JTable table = new JTable(model);
+        panel.add(b1, "West");
+        panel.add(this.label, "Center");
+        panel.add(b2, "East");
+        String[] columns = new String[]{"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+        this.model = new DefaultTableModel((Object[][])null, columns);
+        JTable table = new JTable(this.model);
         JScrollPane pane = new JScrollPane(table);
-
-        this.add(panel, BorderLayout.NORTH);
-        this.add(pane, BorderLayout.CENTER);
-
+        this.add(panel, "North");
+        this.add(pane, "Center");
         this.updateMonth();
+    }
 
+    public static SwingCalendar getInstance() {
+        if (hashcodetest == null && hashcodetest == null) {
+            hashcodetest = new SwingCalendar();
+        }
+
+        return hashcodetest;
     }
 
     void updateMonth() {
-        cal.set(Calendar.DAY_OF_MONTH, 1);
-
-        String month = cal.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.US);
-        int year = cal.get(Calendar.YEAR);
-        label.setText(month + " " + year);
-
-        int startDay = cal.get(Calendar.DAY_OF_WEEK);
-        int numberOfDays = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
-        int weeks = cal.getActualMaximum(Calendar.WEEK_OF_MONTH);
-
-        model.setRowCount(0);
-        model.setRowCount(weeks);
-
+        this.cal.set(5, 1);
+        String month = this.cal.getDisplayName(2, 2, Locale.US);
+        int year = this.cal.get(1);
+        this.label.setText(month + " " + year);
+        int startDay = this.cal.get(7);
+        int numberOfDays = this.cal.getActualMaximum(5);
+        int weeks = this.cal.getActualMaximum(4);
+        this.model.setRowCount(0);
+        this.model.setRowCount(weeks);
         int i = startDay - 1;
-        for (int day = 1; day <= numberOfDays; day++) {
-            model.setValueAt(day, i / 7, i % 7);
-            i = i + 1;
+
+        for(int day = 1; day <= numberOfDays; ++day) {
+            this.model.setValueAt(day, i / 7, i % 7);
+            ++i;
         }
 
     }
